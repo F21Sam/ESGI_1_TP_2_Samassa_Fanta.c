ff2d70253abb68a6bbf609d1d01ea6590538562c
Famamou <samsfanta@gmail.com>
2024-01-30 15:31:27 +0100

    devine le nombre

--- exercice30.c
+++ exercice30.c
@@ -3,9 +3,27 @@
 #include <time.h>
 
 int main() {
-  srand(time(NULL));
-  const int max = 1001;
-  int nombre = rand() % max;
-  /* À vous de jouer */
-  exit(EXIT_SUCCESS);
+    srand(time(NULL));
+    const int max = 1001;
+    int nombre_secret = rand() % max;
+    int tentative, essais = 0;
+
+    printf("Nous avons choisi un nombre entre 0 et 1000\n");
+
+    do {
+        printf("A quel nombre pensez-vous ? ");
+        scanf("%d", &tentative);
+        essais++;
+
+        if (tentative < nombre_secret) {
+            printf("Trop petit.\n");
+        } else if (tentative > nombre_secret) {
+            printf("Trop grand.\n");
+        } else {
+            printf("Bien joué, le nombre était en effet %d\n", nombre_secret);
+            printf("Nombre d'essais : %d\n", essais);
+        }
+    } while (tentative != nombre_secret);
+
+    exit(EXIT_SUCCESS)
 }
